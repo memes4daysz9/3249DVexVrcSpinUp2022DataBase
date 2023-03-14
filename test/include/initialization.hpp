b8639f70e96e50193e2179f85b428d0853427eb0
memes4daysz9 <benj08082008@icloud.com>
2023-03-13 19:27:24 -0700

    Create initialization.hpp

---
+++ test/include/initialization.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include "api.h"
+#include "okapi/api.hpp"
+#include "okapi/impl/device/motor/motorGroup.hpp"
+#include "okapi/api/control/async/asyncVelocityController.hpp"
+
+// Smart ports
+#define leftFrontMotor_PORT 1
+#define leftBackMotor_PORT 2
+#define rightFrontMotor_PORT 3
+#define rightBackMotor_PORT 4
