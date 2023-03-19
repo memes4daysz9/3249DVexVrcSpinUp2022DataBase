c539e11e41b89b0fef19cdea481a05d5244d2039
memes4daysz9 <73305439+memes4daysz9@users.noreply.github.com>
2023-03-19 21:20:39 +0000

    added a "bit" more

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -8,14 +8,31 @@
  * When this callback is fired, it will toggle line 2 of the LCD text between
  * "I was pressed!" and nothing.
  */
+float curve = 0.3;
+
 void on_center_button() {
+	int howManyPressed
 	static bool pressed = false;
 	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "Ha Ha! you thought something is here!, well, shame on you i guess...");
-	} else {
-		pros::lcd::clear_line(2);
+	if (pressed && howManyPressed == 1) {
+		curve = 0.5;
+		pros::lcd::set_text(2, "curve now at 0.5");
+		howManyPressed + 1;
+	} else if (pressed && howManyPressed == 2){
+		curve = 0.7;
+		pros::lcd::set_text(2, "curve now at 0.7"); // the cylcling system that whenever you touch the screen or button, itll change the curve for imeadiate curve change to see what the driver likes
+		howManyPressed + 1;
+	} else if (pressed && howManyPressed == 3){
+		curve = 1;
+		pros::lcd::set_text(2, "curve now at 1");
+		howManyPressed + 1;
+	} else if (pressed && howManyPressed == 4){
+		curve = 0.3;
+		pros::lcd::set_text(2, "curve now at 0.3");
+		howManyPressed = 1;
 	}
+	
+	
 }
 
 /**
@@ -26,7 +43,7 @@ void on_center_button() {
  */
 void initialize() {
 	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
+	pros::lcd::set_text(1, "Hi! tap the screen to get different curves! (base is at 0.3)");
 
 	pros::lcd::register_btn1_cb(on_center_button);
 }
@@ -82,12 +99,47 @@ void opcontrol() {
     float StrafePower;
     float HorizontalPower;
     
+	float PowerCurveVertical;
+	float PowerCurveStrafe;
+	float PowerCurveRotation;
 
     while (true){
+		
+		int cycle = 1
 
-        VerticalPower = MainController.get_analog(E_CONTROLLER_ANALOG_LEFT_Y);
-        StrafePower = MainController.get_analog(E_CONTROLLER_ANALOG_LEFT_X);
-        HorizontalPower = MainController.get_analog(E_CONTROLLER_ANALOG_RIGHT_X);
+		if (MainController.get_digital_new_press(X) && Cycle = 1){
+			MainController.clear;
+			curve = 0.5;
+			MainController.set_text(0,0, "curve is now 0.5");
+			cycle + 1;
+		} else if (MainController.get_digital_new_press(X) && Cycle = 2){
+			MainController.clear;
+			curve = 0.7;
+			MainController.set_text(0,0, "curve is now 0.7");
+			cycle + 1;
+		} else if (MainController.get_digital_new_press(X) && Cycle = 3){ // same thing as above but this is for the controller, making it faster to  compare two modes
+			MainController.clear;
+			curve = 1;
+			MainController.set_text(0,0, "curve is now 1");
+			cycle + 1;
+		} else if (MainController.get_digital_new_press(X) && Cycle = 4){
+			MainController.clear;
+			curve = 0.3;
+			MainController.set_text(0,0, "curve is now 0.3");
+			cycle = 1;
+		}
+
+
+        VerticalPower = MainController.get_analog(E_CONTROLLER_ANALOG_LEFT_Y) * PowerCurveVertical;
+        StrafePower = MainController.get_analog(E_CONTROLLER_ANALOG_LEFT_X) * PowerCurveStrafe; // the way i did this might start up delayed at first movement but will stop imeadiatly after controller stops giving input
+        HorizontalPower = MainController.get_analog(E_CONTROLLER_ANALOG_RIGHT_X)* PowerCurveRotation;
+
+		PowerCurveVertical =  100((1-curve)VerticalPower / 100 + curve (VerticalPower/100)5);
+		PowerCurveStrafe =  100((1-curve)StrafePower / 100 + curve (StrafePower/100)5); // sets a curve for all the joystick axises.... makes the robot move smoother 
+		PowerCurveRotation = 100((1-curve)HorizontalPower / 100 + curve (HorizontalPower/100)5); 
+
+		
+
 
         FrontLeftMotor.move(VerticalPower + StrafePower + HorizontalPower);
         BackLeftMotor.move(VerticalPower - StrafePower + HorizontalPower);
